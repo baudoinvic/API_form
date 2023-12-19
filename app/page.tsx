686564eb94df5fc67b0b5db9027c58f5885f6bde
baudoin <baudoinvicbolingo@gmail.com>
2023-12-19 08:47:09 +0200

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
+"use client";
+
 import Image from 'next/image'
+import { FormEvent, useState } from 'react'
 
 export default function Home() {
+
+const [inputVal, setInputVal] = useState ("")
+
+ const handlesSumbit = (event: FormEvent) => {
+  event.preventDefault();
+ }
+
   return (
      <div>
       <div>
         <h1>Enter your name</h1>
       </div>
-      <form>
-        <input type='text' placeholder='type your name.....'>
+      <form onSubmit={handlesSumbit}>
+        <input type='text' placeholder='type your name' value={inputVal} onChange={(e) => setInputVal(e.target.value)}>
 
         </input>
         <button type='submit'>Predict Data</button>
